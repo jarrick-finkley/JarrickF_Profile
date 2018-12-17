f8000c2da9b6819bb18812d9c428d3cd010d5290
Jarrick Finkley <jarrickfinkley@Jarricks-MacBook-Pro.local>
2018-12-17 13:00:19 -0600

    Project Update

---
+++ js_files/apps.js
@@ -0,0 +1,16 @@
+// $( document ).ready(function() {
+//     console.log('ready!');
+
+//     $('.projects').fadeIn(2000);
+
+//     $(id).on({
+
+//     })
+// });
+
+var about = document.getElementById('about');
+
+about.onclick = function(){
+    window.scroll
+
+};
